3d1a2ff45761338710b1c6c02fbdbc40dbb786a5
kazugufaith <faithmugesia@gmail.com>
2019-06-24 15:17:49 +0300

    added define user-profile

--- insta/views.py
+++ insta/views.py
@@ -8,12 +8,12 @@ from django.contrib.auth.decorators import login_required
 def home(request):
   return render(request,'home.html')
 
-# def user_profile(request):
-#   current_user = request.user
-#   profiles = Profile.objects.filter(user_id=current_user.id)[0:1]
-#   posts = Image.objects.filter(user_id=current_user.id)  
+def user_profile(request):
+  current_user = request.user
+  profiles = Profile.objects.filter(user_id=current_user.id)[0:1]
+  posts = Image.objects.filter(user_id=current_user.id)  
 
-#   return render(request,'insta.html',{"profile_pic":profiles,"posts":posts})
+  return render(request,'insta.html',{"profile_pic":profiles,"posts":posts})
 
 # def feeds(request):  
 #   comments= Comments.objects.all()
