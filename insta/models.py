1152d456c4bab9093d1e723fb4c7a19529f004c2
kazugufaith <faithmugesia@gmail.com>
2019-06-24 15:42:55 +0300

    added a class of profile and defined it

--- insta/models.py
+++ insta/models.py
@@ -2,27 +2,27 @@ from django.db import models
 from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 
-# # Create your models here.
-# class Profile(models.Model):
-#   profile_pic = models.ImageField(upload_to='profile/',default='wink.png')
-#   bio = models.TextField()
-#   user = models.ForeignKey(User,on_delete=models.CASCADE)
+# Create your models here.
+class Profile(models.Model):
+  profile_pic = models.ImageField(upload_to='profile/',default='wink.png')
+  bio = models.TextField()
+  user = models.ForeignKey(User,on_delete=models.CASCADE)
 
-#   def __str__(self):
-#     return self.bio
+  def __str__(self):
+    return self.bio
 
-#   def profile_save(self):
-#     self.save()
+  def profile_save(self):
+    self.save()
 
-#   def profile_delete(self):
-#     self.delete()
-#   class Meta:
-#     ordering=['-bio']
+  def profile_delete(self):
+    self.delete()
+  class Meta:
+    ordering=['-bio']
 
-#   @classmethod
-#   def filter_by_username(cls,user_id):
-#     user = cls.objects.filter(Profile__user=user.id)
-#     return user
+  @classmethod
+  def filter_by_username(cls,user_id):
+    user = cls.objects.filter(Profile__user=user.id)
+    return user
 
 # class Image(models.Model):
 #   image = models.ImageField(upload_to='image/',default='arusha.jpeg')
