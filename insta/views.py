8acb798a790bba17aef8141f7f2b20cbfe4a69c1
kazugufaith <faithmugesia@gmail.com>
2019-06-24 15:38:00 +0300

    defined login for user to sign in or enter their details

--- insta/views.py
+++ insta/views.py
@@ -33,54 +33,54 @@ def search(request):
       message="You did not search any user please input a user name"
       return render(request,"search.html",{"message":message})
 
-# @login_required(login_url='/accounts/login')
-# def comments(request):
-#   current_user=request.user
-#   if request.method == 'POST':
-#     form = NewCommentForm(request.POST)
-#     if form.is_valid():
-#       post = form.save(commit=False)
-#       post.user = current_user
-#       post.save()
-#     return redirect('feed')
+@login_required(login_url='/accounts/login')
+def comments(request):
+  current_user=request.user
+  if request.method == 'POST':
+    form = NewCommentForm(request.POST)
+    if form.is_valid():
+      post = form.save(commit=False)
+      post.user = current_user
+      post.save()
+    return redirect('feed')
 
-#   else:
-#     form = NewCommentForm()
-#   return render(request,'comments.html',{"form":form})
+  else:
+    form = NewCommentForm()
+  return render(request,'comments.html',{"form":form})
 
 
-# @login_required(login_url='/accounts/login')
-# def new_post(request):
-#   current_user = request.user
-#   if request.method == 'POST':
-#     form = NewImageForm(request.POST,request.FILES)
-#     if form.is_valid():
-#       post = form.save(commit=False)
-#       post.user = current_user
-#       post.save()
-#     return redirect('userProfile')
+@login_required(login_url='/accounts/login')
+def new_post(request):
+  current_user = request.user
+  if request.method == 'POST':
+    form = NewImageForm(request.POST,request.FILES)
+    if form.is_valid():
+      post = form.save(commit=False)
+      post.user = current_user
+      post.save()
+    return redirect('userProfile')
 
-#   else:
-#     form = NewImageForm()
-#   return render(request,'new_post.html',{"form":form})
+  else:
+    form = NewImageForm()
+  return render(request,'new_post.html',{"form":form})
 
-# @login_required(login_url='/accounts/login')
-# def profile(request):
-#   current_user = request.user
-#   if request.method == 'POST':
-#     form = NewProfileForm(request.POST,request.FILES)
-#     if form.is_valid():
-#       profile = form.save(commit=False)
-#       profile.user = current_user
-#       profile_pic = form.cleaned_data['profile_pic']
-#       bio = form.cleaned_data['bio']
-#       Profile.objects.filter(user=current_user).update(bio=bio,profile_pic=profile_pic)
-#       profile.save()
-#     return redirect('userProfile')
+@login_required(login_url='/accounts/login')
+def profile(request):
+  current_user = request.user
+  if request.method == 'POST':
+    form = NewProfileForm(request.POST,request.FILES)
+    if form.is_valid():
+      profile = form.save(commit=False)
+      profile.user = current_user
+      profile_pic = form.cleaned_data['profile_pic']
+      bio = form.cleaned_data['bio']
+      Profile.objects.filter(user=current_user).update(bio=bio,profile_pic=profile_pic)
+      profile.save()
+    return redirect('userProfile')
 
-#   else:
-#     form = NewProfileForm()
-#   return render(request,'profile.html',{"form":form})
+  else:
+    form = NewProfileForm()
+  return render(request,'profile.html',{"form":form})
 
 
 
