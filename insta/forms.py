6acb8274b88ca5153c2a28502438dce0026b9930
kazugufaith <faithmugesia@gmail.com>
2019-06-24 15:51:18 +0300

    added a class of newimageform in forms.py file

--- insta/forms.py
+++ insta/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 from .models import Profile,Image,Comments
 
-# class NewImageForm(forms.ModelForm):
-#   class Meta:
-#     model=Image
-#     exclude = ['user','likes','comments']
+class NewImageForm(forms.ModelForm):
+  class Meta:
+    model=Image
+    exclude = ['user','likes','comments']
 
 # class NewProfileForm(forms.ModelForm):
 #   class Meta:
